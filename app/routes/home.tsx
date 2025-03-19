0a28dd4208aaa742cc0976a81fd5210a5f06c166
Robin Augereau <102476446+newtondotcom@users.noreply.github.com>
2025-03-19 15:15:48 +0100

    feat: add a content section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -187,6 +187,24 @@ export default function Home() {
               <Configure />
             </div>
           </div>
+        </section>        
+        
+        <section id="content" className="w-full py-12 md:py-24 lg:py-32">
+          <div className="min-w-full container px-4 md:px-6">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                  Choose what to diplay
+                </h2>
+                <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  Navigate through our app and build your <span className="text-primary">Frame</span> customizez content
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col mx-auto max-w-5xl py-12 items-center">
+              <Configure />
+            </div>
+          </div>
         </section>
 
         <section id="waitlist">
@@ -210,6 +228,7 @@ export default function Home() {
             </div>
           </div>
         </section>
+        
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-muted-foreground">
