04d2cd14e29109f11eab701303e22421a596dd45
Robin Augereau <102476446+newtondotcom@users.noreply.github.com>
2025-03-20 15:52:30 +0100

    style: rounding header + mx auto

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -11,7 +11,7 @@ export default function Header(){
     };
     return (
       <>
-        <header className="fixed top-0 left-0 w-full md:w-[80%] h-16 flex items-center justify-between border-b border-primary bg-white/10 shadow-md z-50 px-4 lg:px-6 backdrop-blur-md">
+        <header className="fixed top-0 left-0 w-full md:w-[80%] h-16 flex items-center justify-between border border-primary bg-white/10 shadow-md z-50 px-4 lg:px-6 backdrop-blur-md mx-auto rounded-3xl">
         <div className="flex items-center gap-2">
           <span className="font-bold text-2xl">FrameIt</span>
         </div>
