dcd528d9f540fd80088ae75d116e41b8f6ffdc9c
newtondotcom <asphalt9fr@gmail.com>
2025-03-20 21:22:37 +0100

    fix: move the footer to the `convinced` section

--- app/components/sections/convinced.tsx
+++ app/components/sections/convinced.tsx
@@ -1,18 +1,20 @@
 import { RainbowButton } from "@/components/ui/rainbow-button";
+import Footer from "@/components/layout/footer";
 
-export default function Convinced(){
-    return (
-        <div className="min-w-full container px-4 md:px-6">
-        <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <div className="space-y-2">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-              Convinced ?
-            </h2>
-          </div>
+export default function Convinced() {
+  return (
+    <div className="min-w-full container px-4 md:px-6 justify-between flex flex-col h-full">
+      <div className="flex flex-col items-center justify-center space-y-4 text-center">
+        <div className="space-y-2">
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+            Convinced ?
+          </h2>
         </div>
         <div className="flex flex-col mx-auto max-w-5xl py-12 items-center">
-            < RainbowButton>Buy it</RainbowButton>
+          <RainbowButton>Buy it</RainbowButton>
         </div>
       </div>
-    )
+      <Footer />
+    </div>
+  );
 }
--- app/components/sections/welcome.tsx
+++ app/components/sections/welcome.tsx
@@ -1,60 +1,59 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRight, ChevronsDown} from "lucide-react";
-import HeroVideoDialog from "@/components/ui/hero-video-dialog";
+import { ArrowRight, ChevronsDown } from "lucide-react";
 import { Link } from "react-router";
+import { RainbowText } from "@/components/ui/rainbow-test";
 
-export default function Welcome(){
-    return(
+export default function Welcome() {
+  return (
     <>
-    <div className="container px-4 md:px-6">
-    <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
-      <div className="flex flex-col justify-center space-y-4">
-        <div className="space-y-2">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-            Redefining Digital Frames with E-Ink Technology
-          </h1>
-          <p className="max-w-[600px] text-muted-foreground md:text-xl">
-            Low power, always-on display that brings your memories to
-            life with the comfort of paper-like viewing.
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 min-[400px]:flex-row">
-          <Link to="/preorder">
-            <Button size="lg">
-              Preorder Now <ArrowRight className="ml-2 h-4 w-4" />
-            </Button>
-          </Link>
-          <Link to="#waitlist">
-            <Button size="lg" variant="outline">
-              Join Waitlist
-            </Button>
-          </Link>
+      <div className="h-screen flex flex-col justify-center container px-4 md:px-6">
+        <div className="flex flex-col justify-center space-y-12">
+          <div className="flex flex-col justify-center space-y-4 w-1/2">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                Redefining Digital Frames with E-Ink Technology
+              </h1>
+              <p className="max-w-[600px] text-muted-foreground md:text-xl">
+                Low power, always-on display that brings your memories to life
+                with the comfort of paper-like viewing.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+              <Link to="/preorder">
+                <Button
+                  size="lg"
+                  className="bg-black dark:bg-white text-secondary"
+                >
+                  Preorder Now <ArrowRight className="ml-2 h-4 w-4" />
+                </Button>
+              </Link>
+              <Link to="#waitlist">
+                <Button
+                  size="lg"
+                  variant="outline"
+                  className="bg-white dark:bg-black"
+                >
+                  Join Waitlist
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center space-y-4 w-full">
+            <div className="flex flex-col items-center">
+              <h3 className="">
+                You thought that E-Ink was only black in white, as in your
+                ereader? Well, we thought so...
+              </h3>
+              <h2 className="text-3xl">
+                but it has
+                <RainbowText>&#xA0;colors !</RainbowText>
+              </h2>
+              <span> Scroll down to discover our site with colors.</span>
+              <ChevronsDown size={300} />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="flex flex-col justify-center space-y-4">
-      You thought that E-Ink was only black in white, as in your ereader ? Well, we thought so...
-      <h2>but it has colors</h2>
-      Scroll down to discover our site with colors.
-      <ChevronsDown size={36}  />
-    </div>
-  </div>
-  <div className="flex flex-col w-full items-center pt-12">
-    <HeroVideoDialog
-      className="block dark:hidden w-1/2"
-      animationStyle="from-center"
-      videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-      thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
-      thumbnailAlt="Hero Video"
-    />
-    <HeroVideoDialog
-      className="hidden dark:block w-1/2"
-      animationStyle="from-center"
-      videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-      thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
-      thumbnailAlt="Hero Video"
-    />
-  </div>
-  </>
-    );
+    </>
+  );
 }
