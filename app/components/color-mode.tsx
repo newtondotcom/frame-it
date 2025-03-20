b69bb1e8505720b13affdf13d18129f202a61e32
Robin Augereau <102476446+newtondotcom@users.noreply.github.com>
2025-03-20 15:37:39 +0100

    style: change color mode button color to primary

--- app/components/color-mode.tsx
+++ app/components/color-mode.tsx
@@ -23,13 +23,13 @@ export default function ThemeToggle() {
       <button onClick={toggleTheme} className="relative cursor-pointer">
         {isDark ? (
           <svg
-            className="w-6 h-6 text-slate-400"
+            className="w-6 h-6 text-primary"
             width="16"
             height="16"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              className="fill-slate-400"
+              className="fill-primary"
               d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z"
             />
             <path
@@ -39,17 +39,17 @@ export default function ThemeToggle() {
           </svg>
         ) : (
           <svg
-            className="w-6 h-6 text-slate-300"
+            className="w-6 h-6 text-primary"
             width="16"
             height="16"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              className="fill-slate-300"
+              className="fill-primary"
               d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z"
             />
             <path
-              className="fill-slate-400"
+              className="fill-primary"
               d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"
             />
           </svg>
