fdccbbe75bee4d805dd2e464efd55d4bf9078249
Robin Augereau <102476446+newtondotcom@users.noreply.github.com>
2025-03-20 15:28:02 +0100

    feat: add a new page to "buy" the product

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
+  route("buy", "routes/buy.tsx"),
   route("mockups", "routes/mockups.tsx"),
   route("mockup/1", "routes/mockup/1.tsx"),
 ] satisfies RouteConfig;
